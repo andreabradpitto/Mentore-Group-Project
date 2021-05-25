96ecefa9c1af7061c4cae680e1cf98fb25e39c71
Andrea Pitto <andreabradpitto@gmail.com>
2021-05-25 17:32:58 +0200

    Name "AddClass" converted to camelCase

--- addClass.py
+++ addClass.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'chooseClass.ui'
+# Form implementation generated from reading ui file 'addClass.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
