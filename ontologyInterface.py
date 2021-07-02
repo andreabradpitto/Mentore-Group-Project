898cbb7cce82c0a2d5f41261caabd31a28c12bfb
Andrea Pitto <andreabradpitto@gmail.com>
2021-07-02 19:48:00 +0200

    partial updates on ontology interface

--- ontologyInterface.py
+++ ontologyInterface.py
@@ -11,6 +11,39 @@ def retrieve_subclasses(ontology: Ontology, parent_class: str) -> list:
     return subcls
 
 
+# This function creates and returns the individual belonging to the class passed as parameter
+#def add_individual_to_ontology(concept, new_class):
+#    indiv = "SIN_" + concept.upper().replace(' ', '')
+#    print(indiv)
+#    # Create the instance of the individual of that class
+#    instance = new_class(indiv)
+#    # Add the isNew property to recognize new individuals
+#    instance.isNew = [True]
+#    # Add keyword1 and keyword2 properties - keyword 2 is always *
+#    print("...adding properties hasKeyword1 and hasKeyword2")
+#    print (keyword1)
+#    print (instance.hasKeyword1)
+#    instance.hasKeyword1=instance.hasKeyword1+keyword1
+#    instance.hasKeyword2 = [locstr("*", lang="en")]
+#    instance.hasQuestion.append(locstr(("Do you like "+ instance.hasKeyword1[0] + "?"), lang="en"))
+#    # Initialize a variable needed to stop the iteration when SEN_GEN individual is found
+#    found = False
+#    for cls in list(onto.classes()):
+#        if found:
+#            break
+#        for ind in cls.instances():
+#            if found:
+#                break
+#            # When SEN_GEN individual is found, add the property hasTopic with value the name of the new individual
+#            if ind.name == "SIN_GEN":
+#                ind.hasTopic.append(instance)
+#                print("...added hasTopic property with value: ", instance.name)
+#                found = True
+#    onto.save(file="ontology/CKB_new.owl", format="rdfxml")
+
+    return instance
+
+
 # Returns the class with the corresponding inputted name
 def retrieve_class(ontology: Ontology, class_name: str):
     for cls in list(ontology.classes()):
@@ -38,24 +71,24 @@ def add_hasSentece_data_property(ontology: Ontology, ontologyPath: str, active_c
 
         if questionFlag == 0:
             if data_type == 0:
-                active_class.hasPositiveSentence.append(
+                active_class.hasPositiveSentence_4.append(
                     locstr(sentence.rstrip(), lang="en"))
             elif data_type == 1:
-                active_class.hasNegativeSentence.append(
+                active_class.hasNegativeSentence_4.append(
                     locstr(sentence.rstrip(), lang="en"))
             else:
-                active_class.hasPositiveAndWait.append(
+                active_class.hasPositiveAndWait_4.append(
                     locstr(sentence.rstrip(), lang="en"))
         else:
             if data_type == 0:
-                active_class.hasQuestion.append(
+                active_class.hasQuestion_4.append(
                     locstr(sentence.rstrip(), lang="en"))
             elif data_type == 1:
-                active_class.hasQuestionGoal.append(
+                active_class.hasQuestionGoal_4.append(
                     locstr(sentence.rstrip(), lang="en"))
             else:
-                active_class.hasQuestionContextual.append(
+                active_class.hasQuestionContextual_4.append(
                     locstr(sentence.rstrip(), lang="en"))
-                active_class.hasQuestionContextualReply.append(
+                active_class.hasQuestionContextualReply_4.append(
                     locstr(answer.rstrip(), lang="en"))
     ontology.save(file=ontologyPath, format="rdfxml")
